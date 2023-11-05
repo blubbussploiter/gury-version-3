5010e63aee3db5f0768d770af66debb287ec93ec
blubbussploiter <130261917+blubbussploiter@users.noreply.github.com>
2023-11-05 11:40:27 -0700

    yeah
    new version of gury, somewhat 2006ish

---
+++ decal.cpp
@@ -0,0 +1,75 @@
+#include "decal.h"
+#include "pvinstance.h"
+
+#include "stdout.h"
+
+//Reflection::PropertyDescriptor<RBX::Decal, RBX::Content> RBX::Decal::prop_texture("Texture", Reflection::TYPE_Content, &RBX::Decal::getTextureContent, &RBX::Decal::setTextureContent, RBX::Decal::properties);
+//Reflection::PropertyDescriptor<RBX::Decal, RBX::NormalId> RBX::Decal::prop_face("Face", Reflection::TYPE_Number, &RBX::Decal::getFace, &RBX::Decal::setFace, RBX::Decal::properties);
+
+RTTR_REGISTRATION
+{
+    rttr::registration::class_<RBX::Decal>("Decal")
+         .constructor<>()
+         .property("Face", &RBX::Decal::getFace, &RBX::Decal::setFace)(rttr::metadata("Type", RBX::Data))
+         .property("Texture", &RBX::Decal::getTextureContent, &RBX::Decal::setTextureContent)(rttr::metadata("Type", RBX::Appearance));
+}
+
+void RBX::Decal::setTextureContent(Content c)
+{
+    if (!c.isEmpty)
+    {
+        if (c.resolve())
+        {
+            GImage image(c.content, c.contentLength);
+
+            if (image.channels > 0)
+            {
+                Texture::Parameters params;
+                params.wrapMode = Texture::WrapMode::CLAMP;
+
+                texture = Texture::fromGImage(getName(), image, TextureFormat::RGBA8, Texture::DIM_2D, params);
+            }
+        }
+    }
+}
+
+void RBX::Decal::fromFile(std::string file, Texture::WrapMode wrap, Texture::InterpolateMode interpolate)
+{
+    wrapMode = wrap;
+    interpolateMode = interpolate;
+    tContent = Content::fromImageFile(file);
+}
+
+void RBX::Decal::render(RenderDevice* rd, RBX::Render::Renderable* p)
+{
+    if (texture.isNull())
+    {
+        setTextureContent(tContent);
+    }
+    else
+    {
+        Render::rawDecal(rd, p, face, getGLid(), GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, 0);
+    }
+}
+
+void RBX::Render::rawDecal(RenderDevice* d, RBX::Render::Renderable* pv, NormalId face, int texture, int sfactor, int dfactor, bool repeat)
+{
+    if (sfactor && dfactor)
+    {
+        glEnable(GL_BLEND);
+        glBlendFunc(sfactor, dfactor);
+    }
+
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    d->setSpecularCoefficient(0.5f);
+    pv->renderFace(d, face);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glDisable(GL_TEXTURE_2D);
+
+    if (sfactor && dfactor)
+    {
+        glBlendFunc(GL_ZERO, GL_ZERO);
+        glDisable(GL_BLEND);
+    }
+}
